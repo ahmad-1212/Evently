bc5af4723131ba2fc99c331918621712b49b45c4
ahmad-1212 <ahmadali.swat333@gmail.com>
2024-10-24 06:42:08 -0700

    add clerk webhook to public route

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-poppins",
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins',
 });
 
 export const metadata: Metadata = {
-  title: "Evently",
-  description: "Evently is a platform for event management.",
+  title: 'Evently',
+  description: 'Evently is a platform for event management.',
   icons: {
-    icon: "/assets/images/logo.svg",
+    icon: '/assets/images/logo.svg',
   },
 };
 
